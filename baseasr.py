58ad80b7aeab28799fe6abcc6fbd90649dc47b19
Rjx0613 <12110613@mail.sustech.edu.cn>
2024-10-11 15:14:23 +0800

    add comment baseasr

--- baseasr.py
+++ baseasr.py
@@ -14,26 +14,39 @@ class BaseASR:
         self.fps = opt.fps # 20 ms per frame
         self.sample_rate = 16000
         self.chunk = self.sample_rate // self.fps # 320 samples per chunk (20ms * 16000 / 1000) 每一帧包含的音频样本数 320
-        self.queue = Queue()
-        self.output_queue = mp.Queue()
+        self.queue = Queue() ## 存储输入的音频帧
+        self.output_queue = mp.Queue() ## 存储输出的音频帧，多进程队列，用于不同进程之间传递音频数据
 
         self.batch_size = opt.batch_size
 
-        self.frames = []
+        self.frames = [] ## 缓存音频帧，用于后面特征提取中从多个帧中生成音频特征
         self.stride_left_size = opt.l
-        self.stride_right_size = opt.r
+        self.stride_right_size = opt.r ## 滑动窗口的大小，用于控制特征提取时需要的历史和未来帧数
         #self.context_size = 10
-        self.feat_queue = mp.Queue(2)
+        self.feat_queue = mp.Queue(2) ## 多进程队列，用于存储提取后的音频特征
 
         #self.warm_up()
 
-    def pause_talk(self): ## 清空队列,暂停说话
+    def pause_talk(self): ## 清空音频帧队列，目的是暂停音频处理，暂停说话
+        """
+        self.queue 清空
+        """
+
         self.queue.queue.clear() 
 
-    def put_audio_frame(self,audio_chunk): #16khz 20ms pcm
+    def put_audio_frame(self,audio_chunk): #16khz 20ms pcm 将PCM格式的16kHz采样率音频放入音频帧队列中，每个帧包含320个样本
+        """
+        self.queue 新增内容
+        """
         self.queue.put(audio_chunk)
 
     def get_audio_frame(self): ## 从队列中获取音频帧，若队列为空则返回一个填充0的空帧，若有父类则需要从父类中获取音频流数据
+        """
+        type 0: 非静音音频
+        type 1: 静音音频
+        type >1: 自定义音频
+        self.queue 丢掉东西
+        """
         try:
             frame = self.queue.get(block=True,timeout=0.01)
             type = 0
@@ -52,9 +65,19 @@ class BaseASR:
         return self.queue.empty()
 
     def get_audio_out(self):  #get origin audio pcm to nerf
+        """
+        self.output_queue 丢掉东西
+        """
         return self.output_queue.get()
     
-    def warm_up(self):
+    def warm_up(self): ## 系统启动时预热音频队列，从输入音频帧队列queue中获取一批音频帧，并存入frames列表中（缓存），同时将这些帧传入output_queue中，为系统提供初始数据
+        """
+        self.queue 丢掉东西
+        self.frames 新增内容
+        self.output_queue 新增内容
+
+        流向： self.queue -> self.frames, self.output_queue, self.output_queue再丢掉
+        """
         for _ in range(self.stride_left_size + self.stride_right_size):
             audio_frame,type=self.get_audio_frame()
             self.frames.append(audio_frame)
